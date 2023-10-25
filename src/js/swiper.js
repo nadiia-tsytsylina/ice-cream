61a33d27160587c026420f37862d0bf091a02d92
Nadiia Tsytsylina <miniova95@gmail.com>
2023-10-25 12:42:30 +0300

    copy files

---
+++ src/js/swiper.js
@@ -0,0 +1,31 @@
+// Initialize Swiper
+
+ 
+    var swiper = new Swiper(".mySwiperGallery", {
+      spaceBetween: 30,
+      centeredSlides: true,
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
+ 
+
+  
+    var swiper = new Swiper(".mySwiperRewiews", {
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      spaceBetween: 30,
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+    });
+  
